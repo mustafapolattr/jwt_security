d4b287431ef82d16210d98e1be27b7ed6d3dc7f1
MPolat <mustfplt4@gmail.com>
2024-08-18 15:52:56 +0300

    Added JwtService

--- src/main/java/com/plat/security/config/JwtService.java
+++ src/main/java/com/plat/security/config/JwtService.java
@@ -1,13 +1,40 @@
 package com.plat.security.config;
 
-
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "e46c4b669960e3ce0476568ecf190479982478be291b8fc56abd5335de995359";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
